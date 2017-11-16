52e09745ef6eeaa49de0974e11d1cb438b68a14d
Aliaksei Datskevich <187672@edu.p.lodz.pl>
2017-11-16 14:41:52 +0100

    pokazuje plik w serwerze

--- Client/src/ClientFTP.java
+++ Client/src/ClientFTP.java
@@ -1,49 +1,48 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class ClientFTP {
+public class ClientFTP { 
 
-	private static Scanner scan, scan1;
+private static Scanner scan, scan1; 
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		System.out.println("Program clienta");
-		Socket fromserver = null;
-		System.out.println("Prosze wpisac port: ");
-		scan = new Scanner(System.in);
-		int port = scan.nextInt();
-		fromserver = new Socket("localhost",port);
-		BufferedReader in  = new BufferedReader(new InputStreamReader(fromserver.getInputStream()));
-	    PrintWriter    out = new PrintWriter(fromserver.getOutputStream(),true);
-	    System.out.println("Wpisz co chcesz otrzymac od serwera: w konsoli klienta\n"
-	    		+ "LIST - wyswitlic liste plikow katalogu serwera\n"
-	    		+ "GET - pobrac plik o nazwie z serwera i zapisac w katalogu klienta\n"
-	    		+ "SHOW - wyswietlic zawartosc pliku w katalogu serwera w konsoli klienta\n"
-	    		+ "QUIT - zamyka klienta\n"
-	    		+ "SHUTDOWN - zamyka serwer\n");
-	    BufferedReader inu = new BufferedReader(new InputStreamReader(System.in));
-	    String fuser,fserver;
-	    while ((fuser = inu.readLine())!=null) {
-	    	out.println(fuser);
-	    	fserver = in.readLine();
-	    	if (fserver.equalsIgnoreCase("Wpisz nazwe pliku:")) {
-	    		out.println(fuser);
-	    	}
-	    	System.out.println(fserver);
-			if (fuser.equalsIgnoreCase("quit")) {
-				break;
-			}
-	    }
-	    out.close();
-	    in.close();
-	    inu.close();
-	    fromserver.close();
-	  }
+public static void main(String[] args) throws UnknownHostException, IOException { 
+System.out.println("Program clienta"); 
+Socket fromserver = null; 
+System.out.println("Prosze wpisac port: "); 
+scan = new Scanner(System.in); 
+int port = scan.nextInt(); 
+fromserver = new Socket("localhost",port); 
+BufferedReader in = new BufferedReader(new InputStreamReader(fromserver.getInputStream())); 
+PrintWriter out = new PrintWriter(fromserver.getOutputStream(),true); 
+System.out.println("Wpisz co chcesz otrzymac od serwera: w konsoli klienta\n" 
++ "LIST - wyswitlic liste plikow katalogu serwera\n" 
++ "GET - pobrac plik o nazwie z serwera i zapisac w katalogu klienta\n" 
++ "SHOW - wyswietlic zawartosc pliku w katalogu serwera w konsoli klienta\n" 
++ "QUIT - zamyka klienta\n" 
++ "SHUTDOWN - zamyka serwer\n"); 
+BufferedReader inu = new BufferedReader(new InputStreamReader(System.in)); 
+String fuser,fserver; 
+while ((fuser = inu.readLine())!=null) { 
+out.println(fuser); 
+fserver = in.readLine(); 
+if (fserver.equalsIgnoreCase("Wpisz nazwe pliku:")) { 
+String name = inu.readLine(); 
+out.println("PLIK " + name); 
+
+} 
+System.out.println(fserver); 
+if (fuser.equalsIgnoreCase("quit")) { 
+break; 
+} 
+} 
+out.close(); 
+in.close(); 
+inu.close(); 
+fromserver.close(); 
+} 
 }
--- Server/src/SimpleServer.java
+++ Server/src/SimpleServer.java
@@ -1,65 +1,107 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Arrays; 
 
-public class SimpleServer {
+public class SimpleServer { 
 
-	public static void main(String[] args) throws IOException {
-		System.out.println("Program server:\n");
-		ServerSocket servers = null;
-		Socket fromclient = null;
-		BufferedReader in;
-		PrintWriter    out;
-		
-		try {
-			servers = new ServerSocket(7);
-		} 
-		catch (IOException e) {
-			System.out.println("Couldn't listen to port 7");
-			System.exit(-1);
-		}
-		
-		try {
-			System.out.print("Waiting for a client...\n");
-			fromclient= servers.accept();
-			System.out.println("Client connected\n");
-		}
-		catch (IOException e) {
-			System.out.println("Can't accept");
-			System.exit(-1);
-		}
-	 
-	
-		in = new BufferedReader(new InputStreamReader(fromclient.getInputStream())); 
-		out = new PrintWriter(fromclient.getOutputStream(),true);
-		 
-		String input;
-	
-		System.out.println("Wait for messages");
-		while ((input = in.readLine()) != null) {
-			if(input.equalsIgnoreCase("list")) {
-				File f = new File("C:\\Users\\ealidat\\Downloads\\");
-				ArrayList<String> names = new ArrayList<String>(Arrays.asList(f.list()));
-				out.println("Folder conatin: " + names);
-				System.out.println(names);
-			}
-			
-			else if (input.equalsIgnoreCase("show")) {
-				out.println("Wpisz nazwe pliku:");
-			}
-			else if (input.equalsIgnoreCase("shutdown")) break;
-//			out.println("Dane wyslane klientem: "+input);
-//			System.out.println(input);
-		}
-		out.close();
-		in.close();
-		fromclient.close();
-		servers.close();
-		}
-	}
+public static void main(String[] args) throws IOException { 
+System.out.println("Program server:\n"); 
+ServerSocket servers = null; 
+Socket fromclient = null; 
+BufferedReader in; 
+PrintWriter out; 
+
+try { 
+servers = new ServerSocket(7); 
+} 
+catch (IOException e) { 
+System.out.println("Couldn't listen to port 7"); 
+System.exit(-1); 
+} 
+
+try { 
+System.out.print("Waiting for a client...\n"); 
+fromclient= servers.accept(); 
+System.out.println("Client connected\n"); 
+} 
+catch (IOException e) { 
+System.out.println("Can't accept"); 
+System.exit(-1); 
+} 
+
+
+in = new BufferedReader(new InputStreamReader(fromclient.getInputStream())); 
+out = new PrintWriter(fromclient.getOutputStream(),true); 
+
+String input; 
+DataOutputStream dout; 
+
+System.out.println("Wait for messages"); 
+while ((input = in.readLine()) != null) { 
+if(input.equalsIgnoreCase("list")) { 
+File f = new File("D:\\Git\\"); 
+ArrayList<String> names = new ArrayList<String>(Arrays.asList(f.list())); 
+out.println("Folder conatin: " + names); 
+} 
+
+else if (input.equalsIgnoreCase("show")) { 
+out.println("Wpisz nazwe pliku:"); 
+} 
+else if (input.startsWith("PLIK") == true) { 
+System.out.println(input); 
+String filename = input.substring(5); 
+System.out.println(filename); 
+// File f=new File("D:\\Git\\" + filename); 
+InputStream input1 = new BufferedInputStream(new FileInputStream("D:\\Git\\" + filename)); 
+byte[] buffer = new byte[8192]; 
+
+try { 
+for (int length = 0; (length = input1.read(buffer)) != -1;) { 
+System.out.write(buffer, 0, length); 
+} 
+} finally { 
+input1.close(); 
+} 
+// FileOutputStream fout=new FileOutputStream(f); 
+// int ch; 
+// String temp; 
+// if(f.exists()) 
+// { 
+// out.println("File Already Exists"); 
+// } 
+// else 
+// { 
+// out.println("SendFile"); 
+// } 
+// do 
+// { 
+// temp=din.readUTF(); 
+// ch=Integer.parseInt(temp); 
+// if(ch!=-1) 
+// { 
+// fout.write(ch); 
+// } 
+// }while(ch!=-1); 
+// fout.close(); 
+// dout.writeUTF("File Send Successfully"); 
+} 
+else if (input.equalsIgnoreCase("shutdown")) break; 
+// out.println("Dane wyslane klientem: "+input); 
+// System.out.println(input); 
+} 
+out.close(); 
+in.close(); 
+fromclient.close(); 
+servers.close(); 
+} 
+}
